261ae5e1cce5118c7f1ccc69aa60863840f6445b
soonwook34 <soonwook34@gmail.com>
2021-06-12 20:42:58 +0900

    모바일 터치 시도2

--- assets/js/paint.js
+++ assets/js/paint.js
@@ -65,8 +65,6 @@ const strokePath = (x, y, color = null) => {
 const onMouseMove = (event) => {
     const x = event.offsetX;
     const y = event.offsetY;
-    console.log(window.pageXOffset + canvas.getBoundingClientRect().left, window.pageYOffset + canvas.getBoundingClientRect().top);
-    console.log(x, y);
     // 그리기 시작
     if (!painting) {
         beginPath(x, y);
@@ -77,6 +75,7 @@ const onMouseMove = (event) => {
 };
 
 const onTouchMove = (event) => {
+    event.preventDefault();
     const x = event.touches[0].pageX - (window.pageXOffset + canvas.getBoundingClientRect().left);
     const y = event.touches[0].pageY - (window.pageYOffset + canvas.getBoundingClientRect().top);
     // 그리기 시작
@@ -131,6 +130,11 @@ export const disableCanvas = () => {
     canvas.removeEventListener("mousedown", startPainting);
     canvas.removeEventListener("mouseup", stopPainting);
     canvas.removeEventListener("mouseleave", stopPainting);
+
+    canvas.removeEventListener("touchmove", onTouchMove);
+    canvas.removeEventListener("touchstart", startPainting);
+    canvas.removeEventListener("touchend", stopPainting);
+    canvas.removeEventListener("touchleave", stopPainting);
 };
 
 // 그리기 적용
